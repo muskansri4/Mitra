9cc3e8b68480c671b4e415c1ab72a2558a9641d8
muskansri4 <muskansrivastava9450@gmail.com>
2023-06-02 17:47:53 +0530

    commit more styling done

--- my-app/src/Components/Projects/ProjectOne/ProjectOneComponent/TvShowDetails.jsx
+++ my-app/src/Components/Projects/ProjectOne/ProjectOneComponent/TvShowDetails.jsx
@@ -14,32 +14,42 @@ export default function TvShowDetails({ tvShow }) {
   return (
     <React.Fragment>
       <div className="title">{tvShow.name}</div>
-      <div className="rating_container">
-        <div className="star_icon">
-          <Rating ratingData={ratingRes} />
-          <span class="star_icon_text">
-            {ratingRes && ratingRes.toFixed(1)} / 5
-          </span>
-        </div>
-        <div className="dot_1"><TiMediaRecord/></div>
-        <div className="air_date">
-          {tvShow.first_air_date.slice(0, 4)}{" "}
-          <span class="air_date_text">On-Air year</span>
-        </div>
-        <div className="dot_2"><TiMediaRecord/></div>
-        <div className="origin">
-          {countries[tvShow.origin_country].name}{" "}
-          <span class="origin_text">Country</span>
-        </div>
-        <div className="dot_3"><TiMediaRecord/></div>
-        <div className="language">
-          {languageNames.of(tvShow.original_language)}
-          <span class="language_text">Language</span>
-        </div>
-        <div className="dot_4"><TiMediaRecord/></div>
-        <div className="media">
-          {tvShow.media_type && tvShow.media_type.toUpperCase()}
-          <span class="media_text">Media</span>
+      <div className="details_container">
+        <div className="rating_container">
+          <div className="star_icon">
+            <Rating ratingData={ratingRes} />
+            <span class="star_icon_text">
+              {ratingRes && ratingRes.toFixed(1)} / 5
+            </span>
+          </div>
+          <div className="dot_1">
+            <TiMediaRecord />
+          </div>
+          <div className="air_date">
+            {tvShow.first_air_date.slice(0, 4)}{" "}
+            <span class="air_date_text">On-Air year</span>
+          </div>
+          <div className="dot_2">
+            <TiMediaRecord />
+          </div>
+          <div className="origin">
+            {countries[tvShow.origin_country].name}{" "}
+            <span class="origin_text">Country</span>
+          </div>
+          <div className="dot_3">
+            <TiMediaRecord />
+          </div>
+          <div className="language">
+            {languageNames.of(tvShow.original_language)}
+            <span class="language_text">Language</span>
+          </div>
+          <div className="dot_4">
+            <TiMediaRecord />
+          </div>
+          <div className="media">
+            {tvShow.media_type && tvShow.media_type.toUpperCase()}
+            <span class="media_text">Media</span>
+          </div>
         </div>
       </div>
       <div className="details">
